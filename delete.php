9a387bf0b505cd028e27333437e6ad9efd6f20da
[Miguel Flandez] <[immfpnvs2@gmail.com]>
2023-11-17 16:39:06 +0800

    OE7

--- delete.php
+++ delete.php
@@ -3,28 +3,34 @@ include("connect.php"); // Include your database connection file
 
 if (isset($_GET['id'])) {
     $user_id = $_GET['id'];
+?>
 
-    // Delete the user with the specified ID
-    $query = "DELETE FROM user_tbl WHERE id = '$user_id'";
-    $result = $conn->query($query);
+    <script type="text/javascript">
+        var confirmDelete = confirm("Are you sure you want to delete this record?");
+        if (confirmDelete) {
+            <?php
+            // Continue with the original PHP code
+            $query = "DELETE FROM user_tbl WHERE id = '$user_id'";
+            $result = $conn->query($query);
 
-    if ($result == TRUE) {
-        echo '<script type="text/javascript">';
-        echo 'alert("Record successfully deleted. . .")';
-        echo '</script>';
-        ?>
-        <script type="text/javascript">
+            if ($result == TRUE) {
+                echo 'alert("Record successfully deleted. . .");';
+            } else {
+                echo 'alert("Error in deleting record. . .");';
+            }
+            ?>
             window.location.href = 'admin_dashboard.php';
-        </script>
-    <?php
-    } else {
-        echo '<script type="text/javascript">';
-        echo 'alert("Error in deleting record. . .")';
-        echo '</script>';
-    }
+        } else {
+            alert("Deletion canceled.");
+            window.location.href = 'admin_dashboard.php';
+        }
+    </script>
+
+<?php
 } else {
     echo '<script type="text/javascript">';
-    echo 'alert("Invalid request. . .")';
+    echo 'alert("Invalid request. . .");';
+    echo 'window.location.href = "admin_dashboard.php";'; // Redirect back to admin_dashboard.php for an invalid request
     echo '</script>';
 }
 ?>
